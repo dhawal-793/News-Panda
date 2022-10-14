48388ab5ea5c40e506e4f0ca42d104fddef4f72f
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 18:15:00 +0530

    Modified SearchNews Component

--- src/components/SearchNews.js
+++ src/components/SearchNews.js
@@ -3,52 +3,53 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
 import Loading from "./Loading";
+import ServerNotFound from "./ServerNotFound";
+import NoReasultsAvailable from "./NoReasultsAvailable";
 
-const SearchNews = (props) => {
-  const capitalize = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
+const SearchNews = ({ query, API_KEY, pagesize, updateProgress }) => {
+  
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
-  const query = props.query.trim();
-
   const [serverNotFound, setServerNotFound] = useState(false);
-  // const [newsType, setNewsType] = useState(props.newsType)
+  query = query.trim();
+  
+
+  const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   const loadNews = async () => {
-    props.updateProgress(15);
-    const url = `https://newsapi.org/v2/top-headlines?&q=${query}&apiKey=${props.API_KEY}&page=${page}&pagesize=${props.pagesize}`;
+    updateProgress(15);
+    const url = `https://newsapi.org/v2/top-headlines?&q=${query}&apiKey=${API_KEY}&page=${page}&pagesize=${pagesize}`;
     setLoading(true);
     let data = await fetch(url);
-    props.updateProgress(40);
+    updateProgress(40);
     if (data.status === 429) {
       setTotalResults(0);
       setLoading(false);
       setServerNotFound(true);
     } else {
       let parsedData = await data.json();
-      props.updateProgress(70);
+      updateProgress(70);
       setArticles(parsedData.articles);
       setTotalResults(parsedData.totalResults);
       setLoading(false);
     }
-    props.updateProgress(100);
+    updateProgress(100);
   };
 
   useEffect(() => {
-    document.title = `NewsPanda - ${capitalize(props.query.trim())}`;
+    document.title = `NewsPanda - ${capitalize(query.trim())}`;
     loadNews();
     // eslint-disable-next-line
   }, []);
 
   const fetchMoreData = async () => {
     setPage(page + 1);
-    const url = `https://newsapi.org/v2/top-headlines?&q=${query}&apiKey=${
-      props.API_KEY
-    }&page=${page + 1}&pagesize=${props.pagesize}`;
+    const url = `https://newsapi.org/v2/top-headlines?&q=${query}&apiKey=${API_KEY
+      }&page=${page + 1}&pagesize=${pagesize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     setArticles(articles.concat(parsedData.articles));
@@ -57,19 +58,8 @@ const SearchNews = (props) => {
 
   return (
     <>
-      {serverNotFound === true ? (
-        <div className="container px-3 my-5" style={{ maxWidth: "60vw" }}>
-          <div className="card text-white bg-dark ">
-            <div className="card-header pl-4">Your Searched "{query}"</div>
-            <div className="card-body text-center">
-              <h5 className="card-title ">Opps...</h5>
-              <p className="card-text ">
-                No News Available Related to the Searched Query,
-              </p>
-              <p className="card-text ">Please try some different keyword...</p>
-            </div>
-          </div>
-        </div>
+      {serverNotFound ? (
+        <ServerNotFound/>
       ) : (
         <div className="container my-8">
           <h2 className="text-center my-4">
@@ -86,25 +76,7 @@ const SearchNews = (props) => {
               <div className="container">
                 <div className="row p-2">
                   {!loading && totalResults < 1 ? (
-                    <div
-                      className="container px-3 my-5"
-                      style={{ maxWidth: "60vw" }}
-                    >
-                      <div className="card text-white bg-dark ">
-                        <div className="card-header pl-4">
-                          You Searched - {query}
-                        </div>
-                        <div className="card-body text-center">
-                          <h5 className="card-title ">Opps...</h5>
-                          <p className="card-text ">
-                            No News available related to searched query,
-                          </p>
-                          <p className="card-text ">
-                            Please try something else...
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                   <NoReasultsAvailable query={query}/>
                   ) : (
                     articles.map((element) => {
                       return (

dbd9868f24ae13ed3227bc4bbec8574ada1eb3b4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-14 18:06:45 +0530

    Modified the Loading Component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import ClipLoader from "react-spinners/ClipLoader";
-import loading from "../gifs/Spinner_black_3.gif";
+import loading from "../assets/gifs/Spinner_black_3.gif";
 
 const Loading = () => {
   return (
     <div className="text-center">
       <img src={loading} alt="Loading" />
-      {/* <ClipLoader color="black" loading="true"  size={50} /> */}
     </div>
   );
 };
